89cb6f3b75047fa4f3ec557353b3d80a821dc051
Hugo Perilleux <hugoperilleux@gmail.com>
2026-04-06 12:43:03 +0200

    :rocket: setup_data avec les nouveaux zip, rappdirs::user_data_dir, création d'un script pour zipper les fichiers vers data_phacochr

--- R/phaco_setup_data.R
+++ R/phaco_setup_data.R
@@ -18,7 +18,7 @@ phaco_setup_data <- function(){
   start_time <- Sys.time()
 
   # Creer le chemin en fonction du systeme d'exploitation (Mac, Windows ou Linux)
-  path_data <- gsub("\\\\", "/", paste0(user_data_dir("phacochr"),"/data_phacochr")) # bricolage pour windows
+  path_data <- gsub("\\\\", "/", paste0(rappdirs::user_data_dir("phacochr"),"/data_phacochr")) # bricolage pour windows
   cat(paste0("\n",colourise("\u2714", fg="green"), " Cr","\u00e9","ation du dossier : ", path_data))
   dir.create(path_data, recursive = T, showWarnings = F)
 
@@ -33,15 +33,15 @@ phaco_setup_data <- function(){
   options(timeout=300)
 
   cat(paste0("\n","\u29D7"," T","\u00e9","l","\u00e9","chargement des donn","\u00e9","es ...","\n"))
-  download.file("https://github.com/phacochr/phacochr_data/raw/main/data_phacochr/phacochr_data_best.zip",
-                paste0(path_data,"/phacochr_data_best.zip"))
-  download.file("https://github.com/phacochr/phacochr_data/raw/main/data_phacochr/phacochr_data_statbel_urbis.zip",
-                paste0(path_data,"/phacochr_data_statbel_urbis.zip"))
+  download.file("https://github.com/phacochr/phacochr_data/raw/main/data_phacochr/phacochr_data_statbel_secteurs.zip",
+                paste0(path_data,"/phacochr_data_statbel_secteurs.zip"))
+  download.file("https://github.com/phacochr/phacochr_data/raw/main/data_phacochr/phacochr_data_autre.zip",
+                paste0(path_data,"/phacochr_data_autre.zip"))
 
   # Test si les donnees ont ete telechargees
   if(sum(
-    file.exists(paste0(path_data,"/phacochr_data_best.zip"),
-                paste0(path_data,"/phacochr_data_statbel_urbis.zip")
+    file.exists(paste0(path_data,"/phacochr_data_statbel_secteurs.zip"),
+                paste0(path_data,"/phacochr_data_autre.zip")
                 )
     ) != 2) {
     options(timeout=60)
@@ -53,12 +53,12 @@ phaco_setup_data <- function(){
 
   # dezippe et supprimer le fichier zip telecharge
   cat(paste0("\n","\u29D7"," D","\u00e9","compression des donn","\u00e9","es"))
-  unzip(paste0(path_data,"/phacochr_data_best.zip"),exdir= path_data)
-  unzip(paste0(path_data,"/phacochr_data_statbel_urbis.zip"),exdir= path_data)
+  unzip(paste0(path_data,"/phacochr_data_statbel_secteurs.zip"),exdir= path_data)
+  unzip(paste0(path_data,"/phacochr_data_autre.zip"),exdir= path_data)
 
   # supression des fichiers .zip
-  file.remove(paste0(path_data,"/phacochr_data_best.zip"))
-  file.remove(paste0(path_data,"/phacochr_data_statbel_urbis.zip"))
+  file.remove(paste0(path_data,"/phacochr_data_statbel_secteurs.zip"))
+  file.remove(paste0(path_data,"/phacochr_data_autre.zip"))
 
   cat(paste0("\r",colourise("\u2714", fg="green")," D","\u00e9","compression des donn","\u00e9","es"))
 
